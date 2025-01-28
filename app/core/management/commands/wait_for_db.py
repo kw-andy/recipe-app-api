4a1dcdce72c200b26380f3cae8cf204b3aeb106f
Andy kw <andy.h.kwok@pm.me>
2025-01-28 18:06:00 +0100

    MAJ - chap 8 - add 2 blank line

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,7 +2,9 @@
 Django command to wait for the database to be available
 """
 
+
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for database"""
